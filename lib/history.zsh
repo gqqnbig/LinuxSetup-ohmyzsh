a9a69563c3eef8f89de9d36adc78afd1fbef6c66
gqqnbig <gqqnb2005@gmail.com>
2025-06-10 09:44:33 +0000

    I don't need such a large history

--- lib/history.zsh
+++ lib/history.zsh
@@ -28,8 +28,6 @@ esac
 
 ## History file configuration
 [ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
-[ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
-[ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
 
 ## History command configuration
 setopt extended_history       # record timestamp of command in HISTFILE
